53b3d509d8d0dacd819e2f10b6860d56021b6482
Aprillion <peter.hozak@gmail.com>
2022-02-19 23:47:23 +0100

    fix fetch problems in CF
    * due to differences between CF and miniflare

--- app/stampy.ts
+++ app/stampy.ts
@@ -5,7 +5,7 @@ import fetch from '~/fetchWithCache'
 
 const reQuestion = /{{[^|]+\|([^}]+)}}/g
 const reRedirect = /#REDIRECT \[\[([^\]]+)\]\]/
-const reAnswerTitle = /canonicalanswer=([^|}]+)/
+const reAnswerTitle = /canonicalanswer=([^|}\n]+)/
 const reAnswerText = /answer=([^|}]+)/
 const reLinkInternal = /\[\[([^\]]+)\]\]/g
 const reLinkExternal = /\[(\S+)\s+([^\]]+)\]/g
@@ -19,12 +19,17 @@ const stampyQuery = (title: string) =>
   `https://stampy.ai/w/api.php?action=query&prop=revisions&rvprop=content&rvslots=*&titles=${title}&format=json&formatversion=2`
 
 const getContent = async (title: string): Promise<string> => {
-  let content = (
-    await (await fetch(stampyQuery(title))).json()
-  ).query.pages[0].revisions[0].slots.main.content.replace(/<!--[\w\W]*?-->/g, '')
-  const redirect = content.match(reRedirect)?.[1]
+  const json = await (await fetch(stampyQuery(title))).json()
+  let content = json.query.pages?.[0].revisions?.[0].slots.main.content?.replace(
+    /<!--[\w\W]*?-->/g,
+    ''
+  )
+  const redirect = content?.match(reRedirect)?.[1]
   if (redirect) {
     content = await getContent(redirect)
+  }
+  if (!content) {
+    content = JSON.stringify(json)
   }
   return content
 }
@@ -32,23 +37,29 @@ const getContent = async (title: string): Promise<string> => {
 export const getInitialQuestions = async () => {
   const initialContent = await getContent('Initial questions')
   const questionList =
-    initialContent.match(reQuestion)?.map((x) => x.replace(reQuestion, '$1')) ?? []
-  const qaList = await Promise.all(
-    questionList.map(async (question, i) => {
-      const questionContent = await getContent(question)
-      const answerTitle = questionContent.match(reAnswerTitle)?.[1]
-      const answerContent = answerTitle ? await getContent(answerTitle) : ''
-      const answerText = answerContent.match(reAnswerText)?.[1]
-      const answerHtml = answerText && parseWikiToHtml(answerText)
+    initialContent?.match(reQuestion)?.map((x) => x.replace(reQuestion, '$1')) ?? []
+  const qaList: {
+    question: string
+    questionContent: string
+    answerContent?: string
+    answerHtml?: string
+  }[] = []
+  // cannot use Promise.all due to `Error: Response closed due to connection limit` from cloudflare
+  // (which is not reproducible locally, so not sure how to fix other than by slowing down)
+  for (const question of questionList) {
+    const questionContent = await getContent(question)
+    const answerTitle = questionContent?.match(reAnswerTitle)?.[1]
+    const answerContent = answerTitle && (await getContent(answerTitle))
+    const answerText = answerContent?.match(reAnswerText)?.[1]
+    const answerHtml = answerText && parseWikiToHtml(answerText)
 
-      return {
-        question,
-        questionContent,
-        answerContent,
-        answerHtml,
-      }
+    qaList.push({
+      question,
+      questionContent,
+      answerContent,
+      answerHtml,
     })
-  )
+  }
 
   return qaList
 }
