74f35d502ba9ef5caa3b79c6f8bd9c4e33d73013
Peter Hozak <peter.hozak@gmail.com>
2022-02-20 12:53:14 +0100

    avoid double build when publishing

--- app/root.tsx
+++ app/root.tsx
@@ -3,11 +3,16 @@ import type {MetaFunction} from 'remix'
 import ogImage from './assets/stampy-ui-preview.png'
 import styles from './root.css'
 
+const title = 'Stampy UI'
+const description = 'Questions and answers about about AI Alignment'
 export const meta: MetaFunction = () => ({
-  title: 'Stampy UI',
-  description: 'Questions and answers about about AI Alignment',
-  'twitter:image': 'summary',
-  'og:image': ogImage,
+  title,
+  description,
+  'og:title': title,
+  'og:description': description,
+  'og:image': `https://stampy-ui.aprillion.workers.dev${ogImage}`,
+  'twitter:image': 'summary_large_image',
+  'twitter:creator': '@aprillion0042',
 })
 export const links: LinksFunction = () => [{rel: 'stylesheet', href: styles}]
 
