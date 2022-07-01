2b0c1c970880a13436b3627eb0993e1d367cf513
ccstan99 <38997995+ccstan99@users.noreply.github.com>
2022-07-01 11:21:20 -0700

    update icons & check encoding gen

--- questions-encodings.mjs
+++ questions-encodings.mjs
@@ -1,22 +1,37 @@
 // server side script called by GitHub Actions to periodically generate encodings stampy.ai wiki questions
 
-// npm install node-fetch @tensorflow/tfjs-node @tensorflow-models/universal-sentence-encoder
+// npm install process node-fetch @tensorflow/tfjs-node @tensorflow-models/universal-sentence-encoder
 import * as tf from '@tensorflow/tfjs-node'
 import * as use from '@tensorflow-models/universal-sentence-encoder'
 import fetch from 'node-fetch'
+import exit from 'process'
 import * as fs from 'fs'
 
-//const filepath = '~/public/assets/'
-const filename = 'stampy-questions-encodings.json'
+const filepath = 'public/assets/'
+const filename = filepath + 'stampy-questions-encodings.json'
+
+let prevNumQs = 0
+if (fs.existsSync(filename)) {
+  const p = JSON.parse(fs.readFileSync(filename, 'utf8'))
+  prevNumQs = p.numQs
+  console.log(`${prevNumQs} questions from in previous generated encodings.`)  
+}
 
 const url =
   'https://stampy.ai/w/api.php?action=ask&query=[[Canonically%20answered%20questions]]|format%3Dplainlist|%3FCanonicalQuestions&format=json'
 const r = await (await fetch(url)).json()
 const response = r.query.results['Canonically answered questions'].printouts.CanonicalQuestions
-console.log(`Fetched ${response.length} questions from the stampy.ai wiki.`)
+console.log(`${response.length} questions fetched from the stampy.ai wiki.`)
 
 const questions = response.map(({fulltext}) => fulltext)
 const questionsNormalized = questions.map((question) => question.toLowerCase())
+const numQs = questions.length
+
+if (prevNumQs == numQs) {
+  console.log(`Number of canonically answered questions has not changed. No new encodings needed.`)
+  // print or return something to let calling script know nothing changed, not to commit?
+  process.exit(0)
+}
 
 const model = await use.load()
 console.log(`Tensorflow's universal sentence encoder model loaded.`)
@@ -24,7 +39,7 @@ console.log(`Tensorflow's universal sentence encoder model loaded.`)
 const encodings = (await model.embed(questionsNormalized)).arraySync()
 console.log(`Questions encoded.`)
 
-let data = JSON.stringify({questions, encodings})
+let data = JSON.stringify({numQs, questions, encodings})
 
 fs.writeFile(filename, data, (err) => {
   if (err) {
