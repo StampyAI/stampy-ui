762d89cfe0b01e015e55e223e884bb32c3c72af3
Aprillion <peter.hozak@gmail.com>
2022-07-05 14:28:32 +0200

    remove custom theming, keep only browser light/dark color scheme

--- app/root.tsx
+++ app/root.tsx
@@ -1,4 +1,4 @@
-import {Links, LiveReload, Meta, Outlet, Scripts, useSearchParams} from '@remix-run/react'
+import {Links, LiveReload, Meta, Outlet, Scripts} from '@remix-run/react'
 import type {MetaFunction, LinksFunction} from '@remix-run/cloudflare'
 import ogImage from '~/assets/stampy-ui-preview.png'
 import styles from '~/root.css'
@@ -17,11 +17,8 @@ export const meta: MetaFunction = () => ({
 export const links: LinksFunction = () => [{rel: 'stylesheet', href: styles}]
 
 export default function App() {
-  const [searchParams] = useSearchParams()
-  const theme = searchParams.get('theme') ?? undefined
-
   return (
-    <html lang="en" className={theme}>
+    <html lang="en">
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width,initial-scale=1" />
