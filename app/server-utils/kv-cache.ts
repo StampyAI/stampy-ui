66b5d59262d1a5d9195e4dcbae2d32fa89f0a6ce
Aprillion <peter.hozak@gmail.com>
2022-10-29 15:46:04 +0200

    more timestamp from KV metadata to value

--- app/server-utils/kv-cache.ts
+++ app/server-utils/kv-cache.ts
@@ -11,15 +11,16 @@ export function withCache<Fn extends (...args: string[]) => Promise<any>>(
 
     const shouldReload = request.url.includes(RELOAD)
     if (!shouldReload) {
-      const {value: cached, metadata} = await STAMPY_KV.getWithMetadata<{timestamp: string}>(key)
+      const cached = await STAMPY_KV.get(key)
 
       if (cached) {
-        const ageInMilliseconds =
-          new Date().getTime() -
-          new Date(metadata?.timestamp ?? '1999-01-01T00:00:00.000Z').getTime()
+        const dataWithTimestamp = JSON.parse(cached)
+        // previous version of cache was just data, now using {data: ..., timestamp: ...} wrapper
+        const {data = dataWithTimestamp, timestamp = '1999-01-01T00:00:00.000Z'} = dataWithTimestamp
+        const ageInMilliseconds = new Date().getTime() - new Date(timestamp).getTime()
         const isLongAge = ageInMilliseconds > 10 * 60 * 1000 // 10 minutes
         console.debug(
-          `Cached data for: ${key} since ${metadata?.timestamp} ${
+          `Cached data for: ${key} since ${timestamp} ${
             isLongAge ? '(will reload)' : '(still fresh)'
           }`
         )
@@ -29,13 +30,15 @@ export function withCache<Fn extends (...args: string[]) => Promise<any>>(
           fetch(reloadUrl)
         }
 
-        return JSON.parse(cached)
+        return data
       }
     }
 
     console.debug(`Fetching data for: ${key}`)
     const data = await fn(...args)
-    await STAMPY_KV.put(key, JSON.stringify(data), {metadata: {timestamp: new Date()}})
+    const dataWithTimestamp = {data, timestamp: new Date()}
+
+    await STAMPY_KV.put(key, JSON.stringify(dataWithTimestamp))
 
     return data
   }) as any
