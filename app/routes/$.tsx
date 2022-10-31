4bcaaf1b0c1d0bfd5792ca27b17ce68f292dc1b6
Aprillion <peter.hozak@gmail.com>
2022-10-31 18:54:03 +0100

    remove redirection to url params to simplify debugging CF issues

--- app/routes/$.tsx
+++
@@ -1,6 +0,0 @@
-import type {LoaderFunction} from '@remix-run/cloudflare'
-import {redirect} from '@remix-run/cloudflare'
-
-export const loader: LoaderFunction = async ({params}) => {
-  return redirect(`/?${params['*']}`)
-}
