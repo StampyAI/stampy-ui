7cd98ef4f95ea4d85ac4dfbfbbdd740dc5ed9259
John Henderson <john.henderson@energyweb.org>
2023-02-11 16:03:19 +0000

    test: question 8392 input-output

--- app/server-utils/url-to-iframe.spec.ts
+++ app/server-utils/url-to-iframe.spec.ts
@@ -2,7 +2,20 @@ import { urlToIframe } from "./url-to-iframe"
 
 describe('urlToIframe', () => {
   it('should handle anchor tags', () => {
-    const text = '';
-    expect(urlToIframe(text)).toBe('result');
+    const input = `
+      <p>There is a Cambrian explosion of approaches to solving alignment. 
+        Click through to the follow-up questions to explore the research directions of groups and individuals in the field, 
+        or browse <a href="https://aisafety.world/">this map</a>:</p>\n
+      <p><a href="https://aisafety.world/">https://aisafety.world/</a></p>\n
+      <p>See also: <a href="https://www.alignmentforum.org/posts/QBAjndPuFbhEXKcCr/my-understanding-of-what-everyone-in-technical-alignment-is">https://www.alignmentforum.org/posts/QBAjndPuFbhEXKcCr/my-understanding-of-what-everyone-in-technical-alignment-is</a></p>\n`;
+
+    const output = `
+      <p>There is a Cambrian explosion of approaches to solving alignment. 
+        Click through to the follow-up questions to explore the research directions of groups and individuals in the field, 
+        or browse <a href="https://aisafety.world/">this map</a>:</p>\n
+      <p><a href="https://aisafety.world/">https://aisafety.world/</a></p>\n
+      <p>See also: <a href="https://www.alignmentforum.org/posts/QBAjndPuFbhEXKcCr/my-understanding-of-what-everyone-in-technical-alignment-is">https://www.alignmentforum.org/posts/QBAjndPuFbhEXKcCr/my-understanding-of-what-everyone-in-technical-alignment-is</a></p>\n`;
+
+    expect(urlToIframe(input)).toBe(output);
   });
 })
