750045cac8c0db0ce203a9290ac9cebd7ed6612d
John Henderson <john.henderson@energyweb.org>
2023-02-11 16:35:08 +0000

    fix(url-to-iframe): href= instead of http

--- app/server-utils/url-to-iframe.ts
+++ app/server-utils/url-to-iframe.ts
@@ -13,12 +13,12 @@ export const urlToIframe = (text: string): string => {
   // const httpsUrlRegex = new RegExp(/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&//=]*)?/gi)
 
   // Regex is from: https://stackoverflow.com/a/26764609
-  const anchorTagRegex = new RegExp(/<a[\s]+([^>]+)>((?:.(?!\<\/a\>))*.)<\/a>/gi)
+  const anchorTagRegex = new RegExp(/<a[\s]+([^>]+)>((?:.(?!<\/a>))*.)<\/a>/gi)
   const matches = text.matchAll(anchorTagRegex)
   let updatedText = text
   for (const match of matches) {
     const fullTag = match[0] 
-    const hrefUrl = match[1].replace('https://','') 
+    const hrefUrl = match[1].replace('href=','') 
     const tagContent = match[2] 
     if (whitelistedHosts.includes(new URL(hrefUrl).host) && hrefUrl === tagContent) {
       updatedText = text.replace(fullTag, `<iframe src="${hrefUrl}"/>`)
