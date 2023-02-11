213b9bcf47f3b6ed83331053c50d0026b9056509
John Henderson <john.henderson@energyweb.org>
2023-02-11 16:29:02 +0000

    feat(url-to-iframe): looking for anchors

--- app/server-utils/url-to-iframe.ts
+++ app/server-utils/url-to-iframe.ts
@@ -8,14 +8,21 @@ export const urlToIframe = (text: string): string => {
     "airtable.com", 
     "aisafety.world"
   ]
+
   // Regex is from: https://stackoverflow.com/a/3809435
-  const httpsUrlRegex = new RegExp(/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&//=]*)?/gi)
-  const matches = text.matchAll(httpsUrlRegex)
+  // const httpsUrlRegex = new RegExp(/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&//=]*)?/gi)
+
+  // Regex is from: https://stackoverflow.com/a/26764609
+  const anchorTagRegex = new RegExp(/<a[\s]+([^>]+)>((?:.(?!\<\/a\>))*.)<\/a>/gi)
+  const matches = text.matchAll(anchorTagRegex)
   let updatedText = text
   for (const match of matches) {
-    const url = match[0] 
-    if (whitelistedHosts.includes(new URL(url).host))
-    updatedText = text.replace(url, `<iframe src="${url}"/>`)
+    const fullTag = match[0] 
+    const hrefUrl = match[1].replace('https://','') 
+    const tagContent = match[2] 
+    if (whitelistedHosts.includes(new URL(hrefUrl).host) && hrefUrl === tagContent) {
+      updatedText = text.replace(fullTag, `<iframe src="${hrefUrl}"/>`)
+    }
   }
   return updatedText
 }
