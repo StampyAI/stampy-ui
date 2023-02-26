07a7ebe5457e4bc9ba04da254758c9ef9a88d2b0
Daniel O'Connell <github@ahiru.pl>
2023-02-26 21:06:56 +0100

    Only show unpublished questions when adding new ones

--- app/routes/questions/search.tsx
+++ app/routes/questions/search.tsx
@@ -3,17 +3,18 @@ import type {LoaderFunction} from '@remix-run/cloudflare'
 export const loader = async ({request}: Parameters<LoaderFunction>[0]) => {
   const url = new URL(request.url)
   const question = url.searchParams.get('question')
+  const onlyLive = url.searchParams.get('onlyLive') == 'true'
 
   if (!question) return []
 
-  return await search(question)
+  return await search(question, onlyLive)
 }
 
-export function search(question: string) {
-  const url = `${NLP_SEARCH_ENDPOINT}/api/search?query=${question}&top=5`
+export function search(question: string, onlyLive: boolean) {
+  const url = `${NLP_SEARCH_ENDPOINT}/api/search?query=${question}&top=5&showLive=${
+    onlyLive ? 1 : 0
+  }`
   return fetch(url).then(async (response) => {
-    const results = await response.json()
-    // Manually filter out questions that are live on site. This will be done via the NLP endpoint at some point
-    return results.filter(({status}: {status: string}) => status != 'Live on site')
+    return await response.json()
   })
 }
