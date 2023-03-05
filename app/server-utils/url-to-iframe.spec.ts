bbb7a85290548f2b6f459252704e1b55ae1efa26
John Henderson <john.henderson@energyweb.org>
2023-03-05 10:31:19 +0000

    chore: apply prettier

--- app/server-utils/url-to-iframe.spec.ts
+++ app/server-utils/url-to-iframe.spec.ts
@@ -1,17 +1,17 @@
-import { urlToIframe } from "./url-to-iframe"
+import {urlToIframe} from './url-to-iframe'
 
 describe('urlToIframe', () => {
   describe('should not convert a markdown link', () => {
     it('whose text does not match url', () => {
       const input = `[this map](https://aisafety.world/)`
-      expect(urlToIframe(input)).toBe(input);
-    });
+      expect(urlToIframe(input)).toBe(input)
+    })
 
     it('whose text matches url but is not whitelisted', () => {
       const url = 'https://example.com/'
       const input = `[${url}](${url})</a>`
-      expect(urlToIframe(input)).toBe(input);
-    });
+      expect(urlToIframe(input)).toBe(input)
+    })
   })
 
   describe('should convert a markdown link', () => {
@@ -19,7 +19,7 @@ describe('urlToIframe', () => {
       const url = 'https://aisafety.world/'
       const input = `[${url}](${url})`
       const output = `<iframe src="${url}" sandbox="allow-scripts allow-same-origin"></iframe>`
-      expect(urlToIframe(input)).toBe(output);
-    });
+      expect(urlToIframe(input)).toBe(output)
+    })
   })
 })
