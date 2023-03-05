d1a1697e5946cd2ab02ee5a2ef60c92dcfd3b287
John Henderson <john.henderson@energyweb.org>
2023-03-05 10:29:16 +0000

    test(url-to-iframe): change to be for markdown

--- app/server-utils/url-to-iframe.spec.ts
+++ app/server-utils/url-to-iframe.spec.ts
@@ -1,24 +1,24 @@
 import { urlToIframe } from "./url-to-iframe"
 
 describe('urlToIframe', () => {
-  describe('should not convert an anchor tag', () => {
-    it('whose href does not match tag content', () => {
-      const input = `<a href="https://aisafety.world/">this map</a>`
+  describe('should not convert a markdown link', () => {
+    it('whose text does not match url', () => {
+      const input = `[this map](https://aisafety.world/)`
       expect(urlToIframe(input)).toBe(input);
     });
 
-    it('whose href matches tag content but is not whitelisted', () => {
+    it('whose text matches url but is not whitelisted', () => {
       const url = 'https://example.com/'
-      const input = `<a href="${url}">${url}</a>`
+      const input = `[${url}](${url})</a>`
       expect(urlToIframe(input)).toBe(input);
     });
   })
 
-  describe('should convert an anchor tag', () => {
-    it('whose href matches tag content and is whitelisted', () => {
-      const url = 'https://example.com/'
-      const input = `<a href="${url}">${url}</a>`
-      const output = `<iframe src="${url}" sandbox="allow-scripts allow-same-origin"/>`
+  describe('should convert a markdown link', () => {
+    it('whose text matches url and is whitelisted', () => {
+      const url = 'https://aisafety.world/'
+      const input = `[${url}](${url})`
+      const output = `<iframe src="${url}" sandbox="allow-scripts allow-same-origin"></iframe>`
       expect(urlToIframe(input)).toBe(output);
     });
   })
