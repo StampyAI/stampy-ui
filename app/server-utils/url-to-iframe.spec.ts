e01eb9828799858799c54a8669c733f88d50df29
John Henderson <john.henderson@energyweb.org>
2023-03-14 08:02:29 +0000

    test(url-to-iframe): extra test cases

--- app/server-utils/url-to-iframe.spec.ts
+++ app/server-utils/url-to-iframe.spec.ts
@@ -2,8 +2,13 @@ import {urlToIframe} from './url-to-iframe'
 
 describe('urlToIframe', () => {
   describe('should not convert a markdown link', () => {
-    it('whose text does not match url', () => {
-      const input = `[this map](https://aisafety.world/)`
+    it.each([
+      {text: 'this map', url: 'https://aisafety.world/'},
+      {text: 'https://aisafety.world/', url: 'https://aisafety.world/?bla=bla'},
+      {text: 'https://aisafety.world/?bla=bla', url: 'https://aisafety.world/'},
+      {text: 'aisafety.world/', url: 'http://aisafety.world/'},
+    ])('whose text does not match url', ({text, url}) => {
+      const input = `[${text}](${url})`
       expect(urlToIframe(input)).toBe(input)
     })
 
@@ -12,11 +17,20 @@ describe('urlToIframe', () => {
       const input = `[${url}](${url})`
       expect(urlToIframe(input)).toBe(input)
     })
+
+    it('whose text matches url but url is not valid', () => {
+      const url = 'aisafety.world/'
+      const input = `[${url}](${url})`
+      expect(urlToIframe(input)).toBe(input)
+    })
   })
 
   describe('should convert a markdown link', () => {
-    it('whose text matches url and is whitelisted', () => {
-      const url = 'https://aisafety.world/'
+    it.each([
+      {url: 'https://aisafety.world/'},
+      {url: 'http://aisafety.world/'},
+      {url: 'https://aisafety.world/?bla=bla'},
+    ])('whose text matches url and host is whitelisted', ({url}) => {
       const input = `[${url}](${url})`
       const output = `<iframe src="${url}" sandbox="allow-scripts allow-same-origin"></iframe>`
       expect(urlToIframe(input)).toBe(output)
