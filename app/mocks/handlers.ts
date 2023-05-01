c979141263324b276df81897d04a1e1943d9e070
John Henderson <stampy@j-henderson.com>
2023-05-01 14:37:52 +0100

    test(stampy.ts): mock coda for question 2400

--- app/mocks/handlers.ts
+++ app/mocks/handlers.ts
@@ -1,31 +1,8 @@
 import {rest} from 'msw'
+import {question2400} from './question-data/question-2400'
+
 export const handlers = [
-  rest.post('/login', (req, res, ctx) => {
-    // Persist user's authentication in the session
-    sessionStorage.setItem('is-authenticated', 'true')
-    return res(
-      // Respond with a 200 status code
-      ctx.status(200)
-    )
-  }),
-  rest.get('/user', (req, res, ctx) => {
-    // Check if the user is authenticated in this session
-    const isAuthenticated = sessionStorage.getItem('is-authenticated')
-    if (!isAuthenticated) {
-      // If not authenticated, respond with a 403 error
-      return res(
-        ctx.status(403),
-        ctx.json({
-          errorMessage: 'Not authorized',
-        })
-      )
-    }
-    // If authenticated, return a mocked user details
-    return res(
-      ctx.status(200),
-      ctx.json({
-        username: 'admin',
-      })
-    )
+  rest.get(new URL(question2400.href).pathname, (req, res, ctx) => {
+    return res(ctx.status(200), ctx.json(question2400))
   }),
 ]
--- app/server-utils/stampy.spec.ts
+++ app/server-utils/stampy.spec.ts
@@ -1,10 +1,18 @@
 import {loadQuestionDetail} from '~/server-utils/stampy'
 import {server} from '~/mocks/setupServers'
+import {question2400} from '~/mocks/question-data/question-2400'
 
 describe('loadQuestionDetail', () => {
+  // Establish API mocking before all tests.
   beforeAll(() => server.listen())
-  it('whose text matches url but url is not valid', async () => {
-    const questionDetail = await loadQuestionDetail('question1')
-    expect(true).toBe(true)
+  // Reset any request handlers that we may add during the tests,
+  // so they don't affect other tests.
+  afterEach(() => server.resetHandlers())
+  // Clean up after the tests are finished.
+  afterAll(() => server.close())
+
+  it('can load question', async () => {
+    const questionDetail = await loadQuestionDetail(undefined, question2400.values['UI ID'])
+    expect(questionDetail).toBeDefined
   })
 })
