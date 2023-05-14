e2591420f27836cbbb22a7872ffae82d27635c2a
Peter Hozak <peter.hozak@gmail.com>
2023-05-14 20:49:44 +0200

    remove hotjar

--- app/root.tsx
+++ app/root.tsx
@@ -92,20 +92,6 @@ function Head({minLogo}: {minLogo?: boolean}) {
       <meta name="color-scheme" content="light dark" />
       <Meta />
       <Links />
-      <script
-        dangerouslySetInnerHTML={{
-          __html: `
-            (function(h,o,t,j,a,r){
-                h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-                h._hjSettings={hjid:3110255,hjsv:6};
-                a=o.getElementsByTagName('head')[0];
-                r=o.createElement('script');r.async=1;
-                r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
-                a.appendChild(r);
-            })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
-            `,
-        }}
-      />
       {minLogo ? (
         <link id="favicon" rel="icon" href="/favicon-min.ico" />
       ) : (
