d8db37aa0433e6e3b1e3c2831f246730266f4c40
John Henderson <stampy@j-henderson.com>
2023-06-04 18:57:39 +0100

    test: remove msw mocks as using undici instead
    Undici is what is provided OOTB from miniflare for fetch mocking.
    https://miniflare.dev/testing/jest#mocking-outbound-fetch-requests

--- app/mocks/handlers.ts
+++
@@ -1,8 +0,0 @@
-import {rest} from 'msw'
-import {question2400} from './question-data/question-2400'
-
-export const handlers = [
-  rest.get(new URL(question2400.href).pathname, (req, res, ctx) => {
-    return res(ctx.status(200), ctx.json(question2400))
-  }),
-]
--- app/server-utils/stampy.spec.ts
+++ app/server-utils/stampy.spec.ts
@@ -1,17 +1,25 @@
 import {loadQuestionDetail} from '~/server-utils/stampy'
-import {server} from '~/mocks/setupServers'
 import {question2400} from '~/mocks/question-data/question-2400'
 
 describe('loadQuestionDetail', () => {
-  // Establish API mocking before all tests.
-  beforeAll(() => server.listen())
-  // Reset any request handlers that we may add during the tests,
-  // so they don't affect other tests.
-  afterEach(() => server.resetHandlers())
-  // Clean up after the tests are finished.
-  afterAll(() => server.close())
-
   it('can load question', async () => {
+    const fetchMock = getMiniflareFetchMock()
+    // Throw when no matching mocked request is found
+    // (see https://undici.nodejs.org/#/docs/api/MockAgent?id=mockagentdisablenetconnect)
+    fetchMock.disableNetConnect()
+
+    // (see https://undici.nodejs.org/#/docs/api/MockAgent?id=mockagentgetorigin)
+    const mockedUrl = new URL(question2400.href)
+    const origin = fetchMock.get(mockedUrl.origin)
+    // (see https://undici.nodejs.org/#/docs/api/MockPool?id=mockpoolinterceptoptions)
+    origin.intercept({method: 'GET', path: mockedUrl.pathname}).reply(200, question2400)
+    origin
+      .intercept({
+        method: 'GET',
+        path: '/apis/v1/docs/fau7sl2hmG/tables/grid-sync-1059-File/rows?useColumnNames=true&sortBy=natural&valueFormat=rich&query=%22Name%22:%22%60%60%602400%60%60%60%22',
+      })
+      .reply(200, {items: [question2400]})
+
     const questionDetail = await loadQuestionDetail(undefined, question2400.values['UI ID'])
     expect(questionDetail).toBeDefined
   })
