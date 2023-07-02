81c90efc7d0807045de55437be2091dc4456bb87
John Henderson <stampy@j-henderson.com>
2023-07-02 15:27:57 +0100

    test: add npm script to run refresh mock data script

--- scripts/refresh-mock-return-data.ts
+++ scripts/refresh-mock-return-data.ts
@@ -2,11 +2,16 @@
  * This is a script to refresh the mock return data for tests
  */
 
-import {loadQuestionDetail} from '~/server-utils/stampy'
+// import {loadQuestionDetail} from '~/server-utils/stampy'
+
+async function main(): Promise<void> {
+  const foo = await fetchQuestionDetails()
+  console.log(foo)
+}
 
 async function fetchQuestionDetails() {
   console.log('works')
   // const questionDetail = await loadQuestionDetail('NEVER_RELOAD', '2400')
 }
 
-await fetchQuestionDetails()
+main()
