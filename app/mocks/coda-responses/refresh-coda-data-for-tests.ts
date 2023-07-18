105e5aaae21f28075a7659af668bec048e2ed1c5
John Henderson <stampy@j-henderson.com>
2023-07-18 22:11:15 +0100

    test(refresh-coda-data): use fetch
    Also updated engine to be >=18 as this is required for node fetch

--- app/mocks/coda-responses/refresh-coda-data-for-tests.ts
+++ app/mocks/coda-responses/refresh-coda-data-for-tests.ts
@@ -3,10 +3,8 @@
  */
 
 import * as fs from 'fs'
-import * as https from 'https'
 import * as path from 'path'
 import * as toml from 'toml'
-import {URL} from 'url'
 
 export const questionIds = [8486]
 
@@ -22,38 +20,15 @@ async function main(): Promise<void> {
 
 const getData = async (questionId: number, codaToken: string) => {
   const options = {
-    hostname: 'coda.io',
-    port: 443,
-    path: `/apis/v1/docs/fau7sl2hmG/tables/grid-sync-1059-File/rows?useColumnNames=true&sortBy=natural&valueFormat=rich&query=%22UI%20ID%22:%22${questionId}%22`,
-    method: 'GET',
     headers: {
       Authorization: `Bearer ${codaToken}`,
     },
   }
-
-  return await httpGet(options)
+  const url = `https://coda.io/apis/v1/docs/fau7sl2hmG/tables/grid-sync-1059-File/rows?useColumnNames=true&sortBy=natural&valueFormat=rich&query=%22UI%20ID%22:%22${questionId}%22`
+  const response = await fetch(url, options)
+  const body = await response.json()
+  return JSON.stringify(body)
 }
-
-const httpGet = (options: string | https.RequestOptions | URL): Promise<string> =>
-  new Promise((resolve, reject) => {
-    const req = https.request(options, (res) => {
-      let data = ''
-
-      res.on('data', (chunk) => {
-        data += chunk
-      })
-
-      res.on('end', () => {
-        resolve(data)
-      })
-    })
-
-    req.on('error', (error) => {
-      reject(error)
-    })
-
-    req.end()
-  })
 
 const writeFile = (questionId: number, data: string): Promise<void> => {
   const filename = `question-${questionId}.json`
