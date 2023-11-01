9741e296374f60e2e9a12c68da1850226ea8fae4
Daniel O'Connell <github@ahiru.pl>
2023-11-01 22:58:35 +0100

    menu for help options

--- app/components/layouts.tsx
+++ app/components/layouts.tsx
@@ -1,7 +1,8 @@
+import {useState} from 'react'
 import {useOutletContext} from '@remix-run/react'
 import logoFunSvg from '../assets/stampy-logo.svg'
 import logoMinSvg from '../assets/stampy-logo-min.svg'
-import {Share, Users, Code, Tag} from './icons-generated'
+import {Share, Tag, Followup} from './icons-generated'
 import CopyLink from './copyLink'
 import type {Context} from '~/root'
 
@@ -9,49 +10,64 @@ const year = new Date().getFullYear()
 
 export const Header = () => {
   const {minLogo, embed} = useOutletContext<Context>()
+  const [showMenu, setShowMenu] = useState(false)
 
   if (embed) return null
 
   return (
-    <header className={minLogo ? 'min-logo' : 'fun-logo'}>
-      <div className="logo-intro-group">
-        <a href="/">
-          <img className="logo" alt="logo" src={minLogo ? logoMinSvg : logoFunSvg} />
-        </a>
-        <div className="intro">
-          {minLogo ? (
-            <>
-              Answering questions about
-              <h1>AI Safety</h1>
-            </>
-          ) : (
-            <>
-              <h1>
-                Welcome to <span className="highlight">stampy.ai</span>!
-              </h1>
-              I can answer questions about artificial general intelligence safety
-            </>
-          )}
+    <header>
+      <div className={['header-bar', minLogo ? 'min-logo' : 'fun-logo'].join(' ')}>
+        <div className="logo-intro-group">
+          <a href="/">
+            <img className="logo" alt="logo" src={minLogo ? logoMinSvg : logoFunSvg} />
+          </a>
+          <div className="intro">
+            {minLogo ? (
+              <>
+                Answering questions about
+                <h1>AI Safety</h1>
+              </>
+            ) : (
+              <>
+                <h1>
+                  Welcome to <span className="highlight">stampy.ai</span>!
+                </h1>
+                I can answer questions about artificial general intelligence safety
+              </>
+            )}
+          </div>
+        </div>
+        <div className="icon-link-group">
+          <CopyLink>
+            <Share />
+            Share link
+          </CopyLink>
+          <a href="/tags/" className="icon-link">
+            <Tag />
+            Tags
+          </a>
+          <a href="#" className="icon-link" onClick={() => setShowMenu(!showMenu)}>
+            <Followup />
+            Help Out
+          </a>
         </div>
       </div>
-      <div className="icon-link-group">
-        <CopyLink>
-          <Share />
-          Share link
-        </CopyLink>
-        <a href="https://get_involved.aisafety.info" className="icon-link">
-          <Users />
-          Get Involved
-        </a>
-        <a href="https://github.com/StampyAI/stampy-ui" className="icon-link">
-          <Code />
-          Help Code
-        </a>
-        <a href="/tags/" className="icon-link">
-          <Tag />
-          Tags
-        </a>
-      </div>
+      {showMenu && (
+        <div className="menu">
+          <a href="https://github.com/StampyAI/stampy-ui" className="menu-link">
+            Code
+          </a>
+          <a href="https://get_involved.aisafety.info" className="menu-link">
+            Write
+          </a>
+          <a
+            href="https://www.every.org/stampy?utm_campaign=donate-link#/donate"
+            className="menu-link"
+          >
+            Donate
+          </a>
+        </div>
+      )}
     </header>
   )
 }
