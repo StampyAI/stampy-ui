ac0fcb7f9ad2bb0cccf683791f16eec1f9394c2a
Peter Hozak <peter.hozak@gmail.com>
2024-02-05 11:18:17 +0000

    don't use react 18 hydrateRoot

--- app/entry.client.tsx
+++ app/entry.client.tsx
@@ -1,4 +1,6 @@
-import {hydrateRoot} from 'react-dom/client'
+// hydrateRoot only worked in dev mode but complained about hydration mismatch in prod build
+// eslint-disable-next-line react/no-deprecated
+import {hydrate} from 'react-dom'
 import {RemixBrowser} from '@remix-run/react'
 
-hydrateRoot(document, <RemixBrowser />)
+hydrate(<RemixBrowser />, document)
