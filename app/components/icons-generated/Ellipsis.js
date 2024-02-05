eb302a124c78a8e163d0a7b533354d12570bc730
Peter Hozak <peter.hozak@gmail.com>
2024-02-05 10:56:21 +0000

    update more libs and regenerate icon files

--- app/components/icons-generated/Ellipsis.js
+++ app/components/icons-generated/Ellipsis.js
@@ -1,16 +1,8 @@
 import * as React from 'react'
 const SvgEllipsis = (props) => (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    viewBox="0 0 448 512"
-    style={{
-      enableBackground: 'new 0 0 448 512',
-    }}
-    xmlSpace="preserve"
-    {...props}
-  >
+  <svg xmlns="http://www.w3.org/2000/svg" xmlSpace="preserve" viewBox="0 0 448 512" {...props}>
     <path
-      d="M336 256c0-8.8 7.2-16 16-16s16 7.2 16 16-7.2 16-16 16-16-7.2-16-16zm-128 0c0-8.8 7.2-16 16-16s16 7.2 16 16-7.2 16-16 16-16-7.2-16-16zm-96 0c0 8.8-7.2 16-16 16s-16-7.2-16-16 7.2-16 16-16 16 7.2 16 16z"
+      d="M336 256c0-8.8 7.2-16 16-16s16 7.2 16 16-7.2 16-16 16-16-7.2-16-16m-128 0c0-8.8 7.2-16 16-16s16 7.2 16 16-7.2 16-16 16-16-7.2-16-16m-96 0c0 8.8-7.2 16-16 16s-16-7.2-16-16 7.2-16 16-16 16 7.2 16 16"
       style={{
         fill: '#333',
       }}
