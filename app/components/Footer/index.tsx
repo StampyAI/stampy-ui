e80364d01f147fa4f08156db91a44f218fe3bbd4
Peter Hozak <peter.hozak@gmail.com>
2024-02-08 17:55:03 +0000

    finish #359 footer

--- app/components/Footer/index.tsx
+++ app/components/Footer/index.tsx
@@ -3,19 +3,23 @@ import './footer.css'
 
 const year = new Date().getFullYear()
 
-const Link = ({to, title}: {to: string, title: string}) => (
-    <div>
-        <a href={to} className="unstyled">{title}</a>
-    </div>
+const Link = ({to, title}: {to: string; title: string}) => (
+  <div>
+    <a href={to} className="unstyled" target="_blank" rel="noreferrer">
+      {title}
+    </a>
+  </div>
 )
 
 export const FooterBar: FunctionComponent = () => {
   return (
     <div className="footer" key="footer">
-        <hr />
+      <hr />
       <div className="footer-grid">
         <div>
-          <p className="footer-brand">AISafety.info</p>
+          <p className="footer-brand">
+            <a href="/">AISafety.info</a>
+          </p>
           <div className="footer-description">
             We're a global team of volunteers from various disciplines who believe AI poses an grave
             risk of extinction to humanity.
@@ -24,21 +28,23 @@ export const FooterBar: FunctionComponent = () => {
 
         <div className="help">
           <h5>Help out</h5>
-          <Link to="https://www.every.org/stampy?utm_campaign=donate-link#/donate" title="Donate"/>
-          <Link to="https://github.com/StampyAI/stampy-ui" title="Code"/>
-          <Link to="https://get_involved.aisafety.info/" title="Write"/>
-          <Link to="https://discord.com/invite/Bt8PaRTDQC" title="Join us on Discord"/>
+          <Link to="https://www.every.org/stampy?utm_campaign=donate-link#/donate" title="Donate" />
+          <Link to="https://github.com/StampyAI/stampy-ui" title="Code" />
+          <Link to="https://get_involved.aisafety.info/" title="Write" />
+          <Link to="https://discord.com/invite/Bt8PaRTDQC" title="Join us on Discord" />
         </div>
         <div className="partners">
           <h5>Partner projects</h5>
-          <Link to="https://www.aisafety.com/" title="AIsafety.com"/>
-          <Link to="https://alignment.dev/" title="Alignment Ecosystem Development"/>
+          <Link to="https://www.aisafety.com/" title="AIsafety.com" />
+          <Link to="https://alignment.dev/" title="Alignment Ecosystem Development" />
         </div>
       </div>
       <div className="copyright grey">
-        <a href="https://coda.io/d/AI-Safety-Info-Dashboard_dfau7sl2hmG/Copyright_su79L#_luPMa" className="unstyled">
-          © stampy.ai, 2022 - {year}
-        </a>
+        <br />
+        <Link
+          to="https://coda.io/d/AI-Safety-Info-Dashboard_dfau7sl2hmG/Copyright_su79L#_luPMa"
+          title={`© stampy.ai, 2022 - ${year}`}
+        />
       </div>
     </div>
   )
