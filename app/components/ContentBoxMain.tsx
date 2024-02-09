d7b065c52e82b5c5639f1d9c2fdcf6a119f2aabf
Peter Hozak <peter.hozak@gmail.com>
2024-02-09 10:43:21 +0000

    make start here clickable again

--- app/components/ContentBoxMain.tsx
+++ app/components/ContentBoxMain.tsx
@@ -4,6 +4,12 @@ export const ContentBoxMain = () => {
   return (
     <div className="main-container primary bordered">
       <div className="content-box-description primary">
+        <img
+          loading="lazy"
+          src="https://cdn.builder.io/api/v1/image/assets/TEMP/23c2daf90ad8594e0b7c3748f83751e577ae52f13fcb7f897a573cb95458884c?apiKey=f1073757e44b4ccd8d59791af6c41a77&"
+          className="content-box-right-icon"
+          alt="AI Safety Image"
+        />
         <h2>
           <div className="white">New to AI Safety?</div>
           <div className="teal-200">
@@ -18,12 +24,6 @@ export const ContentBoxMain = () => {
           </div>
         </a>
       </div>
-      <img
-        loading="lazy"
-        src="https://cdn.builder.io/api/v1/image/assets/TEMP/23c2daf90ad8594e0b7c3748f83751e577ae52f13fcb7f897a573cb95458884c?apiKey=f1073757e44b4ccd8d59791af6c41a77&"
-        className="content-box-right-icon"
-        alt="AI Safety Image"
-      />
     </div>
   )
 }
