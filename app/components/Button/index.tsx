72855b749a14f00adbf4dc761bf7357758c310db
Melissa Samworth <melissasamworth@gmail.com>
2024-02-11 16:24:28 +0000

    Button

--- app/components/Button/index.tsx
+++ app/components/Button/index.tsx
@@ -2,23 +2,16 @@ import {ReactNode} from 'react'
 import './button.css'
 
 type ButtonProps = {
-  action?: string | (() => void)
-  children?: ReactNode
-  className?: string
+    action?: string | (() => void)
+    children?: ReactNode
+    className?: string
 }
 const Button = ({children, action, className}: ButtonProps) => {
-  if (typeof action === 'string') {
-    return (
-      <a href={action} className={className}>
-        {children}
-      </a>
-    )
-  }
-  return (
-    <button className={className} onClick={action}>
-      {children}
-    </button>
-  )
+    const classNamea = "button " + (className||"")
+    if (typeof(action) === 'string') {
+        return <a href={action} className={classNamea}>{children}</a>
+    }
+    return <button className={classNamea} onClick={action}>{children}</button>
 }
 
 export default Button
