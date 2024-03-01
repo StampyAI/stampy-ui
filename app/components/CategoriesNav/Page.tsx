9a1af287c88dced0369afb91bb96c36cd96f39f8
Buddy <0buddy.ne@gmail.com>
2024-03-01 08:44:23 +0200

    categories Page

---
+++ app/components/CategoriesNav/Page.tsx
@@ -0,0 +1,34 @@
+import {useState} from 'react'
+import {Link} from '@remix-run/react'
+import {SearchInput} from '../SearchInput/Input'
+import {Tag as TagType} from '~/server-utils/stampy'
+import {tagUrl} from '~/routesMapper'
+import './menu.css'
+
+interface CategoriesPageProps {
+  /**
+   * Categories List
+   */
+  categories: TagType[]
+}
+
+export const CategoriesPage = ({categories}: CategoriesPageProps) => {
+  const [search, onSearch] = useState('')
+  return (
+    <div className={'categoriesPage col-12'}>
+      <h1>Categories</h1>
+      <div>
+        <SearchInput onChange={onSearch} placeholderText="Filter by keyword" />
+      </div>
+      <div className={'categories-results'}>
+        {categories
+          .filter((tag) => tag.name.toLowerCase().includes(search.toLowerCase()))
+          .map(({tagId, name, questions}) => (
+            <Link key={tagId} to={tagUrl({tagId, name})} className={'categoryTitle'}>
+              {name} ({questions.length})
+            </Link>
+          ))}
+      </div>
+    </div>
+  )
+}
