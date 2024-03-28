d0c73784b144a41dfc959073aff16910a563a9fd
buddy <0buddy.ne@gmail.com>
2024-03-28 08:59:44 +0200

    minor styling ChatBot

--- app/components/Nav/index.tsx
+++ app/components/Nav/index.tsx
@@ -1,6 +1,7 @@
 import {Link} from '@remix-run/react'
 import {MenuItem} from '~/components/Menu'
 import OpenBookIcon from '~/components/icons-generated/OpenBook'
+import BotIcon from '~/components/icons-generated/Bot'
 import AISafetyIcon from '~/components/icons-generated/Aisafety'
 import ArticlesDropdown from '~/components/ArticlesDropdown'
 import type {TOCItem} from '~/routes/questions.toc'
@@ -28,6 +29,12 @@ export const Nav = ({toc, categories}: NavProps) => {
             id="showArticles"
           />
           <ArticlesDropdown toc={toc} categories={categories || []} />
+          <MenuItem
+              primary={true}
+              link="https://chat.aisafety.info"
+              icon={<BotIcon />}
+              text="Stampy chatbot"
+          />
           <li className="top-menu-item">
             <div className="top-menu-divider"></div>
           </li>
