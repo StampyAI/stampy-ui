33cf651ebfea73e562255d141032ad7b8de43367
Melissa Samworth <melissasamworth@gmail.com>
2024-10-23 14:00:49 -0400

    pixel perfect index page

--- app/routes/how-can-i-help._index.tsx
+++ app/routes/how-can-i-help._index.tsx
@@ -10,10 +10,10 @@ export const meta: MetaFunction = () => {
 export default function HowCanIHelp() {
   return (
     <Page>
-      <div className="page-body padding-top-40">
-        <div className="howcanihelp-header padding-bottom-104">
-          <h1 className="teal-500">How can I help with AI Safety?</h1>
-          <p className="large-reading padding-left-80">
+      <div className="page-body padding-top-56">
+        <div className="flexbox padding-bottom-80">
+          <h1 className="teal-500 col-7">How can I help with<br></br> AI safety?</h1>
+          <p className="large col-5">
             The AI safety movement is still relatively new, and your actions could have significant
             impact. Here's what you can do:
           </p>
