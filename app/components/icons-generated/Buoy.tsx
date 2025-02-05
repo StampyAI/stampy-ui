669465e30613a1b21e130d8fa85c6ad7d3e4efd2
Olivier Coutu <coutu.olivier@gmail.com>
2025-02-05 13:59:20 -0500

    Properly generated buoy.svg

--- app/components/icons-generated/Buoy.tsx
+++ app/components/icons-generated/Buoy.tsx
@@ -1,11 +1,9 @@
 import type {SVGProps} from 'react'
 const SvgBuoy = (props: SVGProps<SVGSVGElement>) => (
   <svg xmlns="http://www.w3.org/2000/svg" width={24} height={24} fill="none" {...props}>
-    <circle cx="12" cy="12" r="5" stroke="#1D9089" />
-    <circle cx="12" cy="12" r="9" stroke="#1D9089" />
-    <path d="M3.5 12H7M17 12H20.5" stroke="#1D9089" />
-    <path d="M12 16.5V20" stroke="#1D9089" />
-    <path d="M12 4V7.5" stroke="#1D9089" />
+    <circle cx={12} cy={12} r={5} stroke="#1D9089" />
+    <circle cx={12} cy={12} r={9} stroke="#1D9089" />
+    <path stroke="#1D9089" d="M3.5 12H7m10 0h3.5M12 16.5V20M12 4v3.5" />
   </svg>
 )
 export default SvgBuoy
