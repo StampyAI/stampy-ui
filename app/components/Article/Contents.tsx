dcbb492088960aea245e75f2a1876e16eb6187d7
Olivier <coutu.olivier@gmail.com>
2025-04-14 10:38:56 -0400

    Shorten enumeration of Glossary exemption conditions
    Co-authored-by: Peter Hozák <peter.hozak@gmail.com>

--- app/components/Article/Contents.tsx
+++ app/components/Article/Contents.tsx
@@ -72,10 +72,9 @@ const updateTextNodes = (el: Node, textProcessor: (node: Node) => Node) => {
     // Check if node has a parentElement we can use closest() on
     return Boolean(
       // Skip links, headings, and existing glossary entries
-      node.parentElement?.closest('a') ||
-        node.parentElement?.closest('h1, h2, h3, h4, h5') ||
-        node.parentElement?.closest('.glossary-entry') ||
-        node.parentElement?.closest('.glossary-popup')
+      node.parentElement?.closest(
+        'a, h1, h2, h3, h4, h5, .glossary-entry, .glossary-popup'
+      )
     )
   }
 
