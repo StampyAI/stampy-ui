4cc26a82975218df6d429d482d7fe94237917957
Algon33 <algon33.ar@gmail.com>
2025-08-18 16:35:05 +0100

    Added donation button and icon to menu

--- app/components/Nav/index.tsx
+++ app/components/Nav/index.tsx
@@ -4,12 +4,15 @@ import OpenBookIcon from '~/components/icons-generated/OpenBook'
 import BuoyIcon from '~/components/icons-generated/Buoy'
 import AISafetyIcon from '~/components/icons-generated/Aisafety'
 import BotIcon from '~/components/icons-generated/Bot'
+import Hand from '~/components/icons-generated/Hand' 
+import DonateHeart from '~/components/icons-generated/DonateHeart' 
 import ArticlesDropdown from '~/components/ArticlesDropdown'
 import {ThemeToggle} from '~/components/ThemeToggle'
 import type {TOCItem} from '~/routes/questions.toc'
 import type {Tag} from '~/server-utils/stampy'
 import './nav.css'
 import Search from '../search'
+import Donate from '~/routes/how-can-i-help.donate'
 
 export interface NavProps {
   toc: TOCItem[]
@@ -43,6 +46,12 @@ export const Nav = ({toc, categories}: NavProps) => {
             icon={<BotIcon />}
             text="AI Safety Chatbot"
             reload
+          />
+          <MenuItem
+            primary={true}
+            link="https://www.every.org/aisafetyinfo/f/keep-stampy-alive#/donate/card"
+            icon={<DonateHeart />} // Temporary - replace with donate icon later
+            text="Donate"
           />
           <li className="top-menu-item">
             <div className="top-menu-divider"></div>
