d979caeeb8b2fafa12649dc66b04eece841418bf
jrhender <stampy@j-henderson.com>
2025-09-15 07:14:20 -0700

    fix(answerDetailsOnSite): typescript errror

--- app/routes/questions.answerDetailsOnSite.ts
+++ app/routes/questions.answerDetailsOnSite.ts
@@ -5,7 +5,7 @@ export const loader = async ({request}: LoaderFunctionArgs) => {
   const searchParams = new URL(request.url).searchParams
   const nextPage = searchParams.get('nextPage')
   if (nextPage !== null) {
-    return await loadMoreAnswerDetails(request, nextPage || null)
+    return await loadMoreAnswerDetails(request, nextPage)
   }
   const {data, timestamp} = await loadOnSiteAnswers(request)
   return {data: {questions: data, nextPageLink: null}, timestamp}
